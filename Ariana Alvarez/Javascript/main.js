cc03c08b28e1b5c6533597b8c612cb83757b4a04
Ariana Álvarez <aalvarez9712@gmail.com>
2024-02-05 12:11:51 -0300

    Agregados archivos de Ariana

---
+++ Ariana Alvarez/Javascript/main.js
@@ -0,0 +1,23 @@
+var miEdad = 25;
+const miNombre = "Ariana";
+
+var suma = 5 + 4;
+console.log(suma);
+
+if(miEdad == 26){
+    console.log("Tu edad es de 26");
+}else if(miEdad == 25){
+    console.log("Tu edad es 25");
+}else{
+    console.log("Tu edad no es ni 25 ni 26");
+}
+
+while(miEdad<30){
+    console.log("Tu edad es "+miEdad);
+    miEdad = miEdad +1;
+}
+
+for(let contador = 0;contador<4;contador++){
+    console.log("el contador esta en "+contador);
+
+}
